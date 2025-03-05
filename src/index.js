aa2d63ec78c5102728e056ba2cd1b169d48c543d
arcane <arcanesir.neo@gmail.com>
2025-03-05 18:56:21 +0100

    new architecture

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
-// import { App } from './components/App/App';
+import { App } from './components/App/App';
 import './index.css';
 import { persistor, store } from './redux/store';
-import { AppTest } from 'components/App/AppTest';
+// import { AppTest } from 'components/App/AppTest';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersistGate persistor={persistor} loading={null}>
       <Provider store={store}>
         <BrowserRouter basename="/product-list-with-cart">
-          {/* <App /> */}
-          <AppTest />
+          <App />
+          {/* <AppTest /> */}
         </BrowserRouter>
       </Provider>
     </PersistGate>
